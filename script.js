cb6edaebdce23bfb7adffd606142e4d2833bd22b
Alaa Hesham <alaaabdulla2001@gmail.com>
2023-08-26 13:38:29 +0300

    finished dashboard

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const body = document.querySelector("body"),
     modeSwitch = document.querySelector(".toggle-switch"),
     modeText = document.querySelector(".mode-text"),
     searchIcon = document.querySelector(".bx-search"),
-    ratio = document.querySelector(".card .ratio-container input[type='range']");
+    ratio = document.querySelector(".card .ratio-container input[type='range']"),
+    sidebarFields = document.querySelectorAll(".sidebar li a");
 
 
 //------------ tickets overview/graph -------------
@@ -12,7 +13,7 @@ var options = {
         data: [20, 220, 180, 450, 350, 500, 300]
     }],
     chart: {
-        height: 350,
+        height: 270,
         type: 'area',
         foreColor: getComputedStyle(document.documentElement).getPropertyValue('--text-color'),
         toolbar: {
@@ -58,23 +59,12 @@ var options2 = {
     series: [40, 22, 13, 25],
     chart: {
         type: 'donut',
-
+        height: 220
     },
-    responsive: [{
-        breakpoint: 480,
-        options: {
-            chart: {
-                width: 200
-            },
-            legend: {
-                position: 'bottom'
-            }
-        }
-    }],
     dataLabels: {
         enabled: false
     },
-    colors: ['#0db8ed', '#FF9800', '#E91E63', '#2E93fA'],
+    colors: ['#0db8ed', '#FF9800', '#E91E63', '#4285f4'],
     legend: {
         show: false,
     },
@@ -92,7 +82,75 @@ var options2 = {
 var donutChart = new ApexCharts(document.querySelector("#chart2"), options2);
 donutChart.render();
 
-// Events
+//------------------------ Bar chart ---------------------
+
+var options3 = {
+    series: [{
+        name: 'Within One Day',
+        data: [83, 44]
+    }, {
+        name: 'Within Two Days',
+        data: [7, 39]
+    }, {
+        name: 'Within Four Days',
+        data: [5, 11]
+    }, {
+        name: 'More Than Four Days',
+        data: [4, 6]
+    }],
+    chart: {
+        type: 'bar',
+        height: 190,
+        stacked: true,
+        stackType: '100%',
+        toolbar: {
+            show: false,
+        },
+        foreColor: getComputedStyle(document.documentElement).getPropertyValue('--text-color'),
+    },
+    plotOptions: {
+        bar: {
+            horizontal: true,
+        },
+    },
+    stroke: {
+        width: 1,
+        colors: [getComputedStyle(document.documentElement).getPropertyValue('--body-color')],
+    },
+    xaxis: {
+        categories: ["Response Time", "Resolution Time"],
+    },
+    yaxis: {
+        labels: {
+            style: {
+                fontSize: '1rem',
+            }
+        }
+    },
+    tooltip: {
+        y: {
+            formatter: function (val) {
+                return val + "K"
+            }
+        }
+    },
+    fill: {
+        opacity: 1
+
+    },
+    legend: {
+        position: 'top',
+        horizontalAlign: 'left',
+        offsetX: 40
+    },
+    colors: ['#4285f4', '#0db8ed', '#FF9800', '#E91E63'],
+
+};
+
+var barChart = new ApexCharts(document.querySelector("#chart3"), options3);
+barChart.render();
+
+// ----------------------- Events ---------------------------
 
 modeSwitch.addEventListener("click", () => {
     body.classList.toggle("dark");
@@ -105,40 +163,66 @@ modeSwitch.addEventListener("click", () => {
         modeText.innerText = "Dark Mode";
         textColor = "#000";
         bodyColor = "#fff";
-
-
     }
 
     chart.updateOptions({
         chart: {
-            height: 350,
-            type: 'area',
             foreColor: textColor,
-            toolbar: {
-                show: false
-            }
         }
     });
-    console.log(bodyColor);
+
     donutChart.updateOptions({
         stroke: {
             colors: bodyColor,
         }
     });
 
+    barChart.updateOptions({
+        chart: {
+            foreColor: textColor,
+        },
+        stroke: {
+            width: 1,
+            colors: [bodyColor],
+        }
+    });
 });
 
 searchIcon.onclick = () => {
     document.querySelector(".main-header input[type=text]").focus();
 };
 
-body.onload = () => {
-    document.querySelector("a.dashboard-section").focus();
-}
+sidebarFields.forEach(sidebarField => {
+    sidebarField.onfocus = () => {
+        sidebarFields.forEach(sidebarField => {
+            sidebarField.classList.remove("selected");
+        });
+        sidebarField.classList.add("selected");
+    };
+});
 
-
-ratio.oninput = () => {
+function positionRatioBox() {
     let ratioBox = document.querySelector(".card .ratio-container output");
     ratioBox.innerText = ratio.value + "%";
-    ratioBox.style.transform = "translateX(" + (2 * ratio.value - 15) + "px)";
+    ratioBox.style.left = (ratio.offsetWidth / 100 * ratio.value - 15) + "px";
+}
+
+body.onload = async () => {
+    while (ratio.value < 33) {
+        ratio.value++;
+        await delay(500 / 33);
+        positionRatioBox();
+    }
+}
+
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+window.onresize = () => {
+    positionRatioBox();
+}
+
+ratio.oninput = () => {
+    positionRatioBox();
 }
